763fa7bd966254f9b9b62a3036bd38eaf779c507
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-03-22 00:16:16 -0300

    Add correct permissions and authentication classes

--- apps/applications/api/v1/viewsets.py
+++ apps/applications/api/v1/viewsets.py
@@ -1,8 +1,12 @@
 from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 from apps.applications.api.v1.serializers import PlanSerializer
 from apps.applications.models import Plan
 
-class PlanViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class PlanViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
